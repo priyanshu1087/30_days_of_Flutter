a912e9fd311cccb549674e0f21942fa72e1c62b9
Priyanshu Das <priyanshu66662@gmail.com>
2023-03-17 22:11:31 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+
+@override
+Widget build(BuildContext context){
+  return MaterialApp(
+    home: Material(
+    child:Center(
+    child: Container(
+    child: Text("Hi , this is Priyanshu.......<3:)"),
+  ),
+    ),
+  ),
+  );
+}
+}
